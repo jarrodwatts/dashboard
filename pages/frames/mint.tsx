408199a6802b49861047cde4e738671dfb9d98b8
easyrun42 <declaimyy@gmail.com>
2024-01-30 21:51:47 +0000

    quick button

--- pages/frames/mint.tsx
+++ pages/frames/mint.tsx
@@ -27,6 +27,27 @@ export default function FramesMint() {
         />
         <meta property="fc:frame:button:1" content="Get started" />
       </Head>
+      <button
+        onClick={() => {
+          fetch(
+            "https://thirdweb-gp35zxd89.thirdweb-preview.com/api/mint?type=start",
+            {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify({
+                trustedData: {
+                  messageBytes:
+                    "0a42080d10c4aa0118c6d1922e20018201320a12687474703a2f2f6578616d706c652e636f6d10011a1a08c4aa0112141fd48ddc9d5910046acfa5e1b91d253763e320c31214230a1291ae8e220bf9173d9090716981402bdd3d18012240f08c907486afe1c3311565b7a27c1f0011c74bd22ba167abe8ba30a35e808cbeae674aef7b74d3161c6186e48e3cc4d843c5ec9dc1dce9c6b71547adcc02c90c28013220196a70ac9847d59e039d0cfcf0cde1adac12f5fb447bb53334d67ab18246306c",
+                },
+              }),
+            },
+          );
+        }}
+      >
+        123
+      </button>
     </Fragment>
   );
 }
