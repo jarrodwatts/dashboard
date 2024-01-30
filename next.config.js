d27fc57bc343d648fa37e1f190096ec7754b89a0
easyrun42 <declaimyy@gmail.com>
2024-01-30 21:48:42 +0000

    allow origin

--- next.config.js
+++ next.config.js
@@ -91,6 +91,22 @@ const moduleExports = {
         source: "/(.*)",
         headers: securityHeaders,
       },
+      {
+        source: "/api/mint",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
     ];
   },
   async redirects() {
